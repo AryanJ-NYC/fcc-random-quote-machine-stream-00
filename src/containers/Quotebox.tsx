98de0a1743b54bbfce81e4f4d63e9c0d27488bdb
Aryan Jabbari <Aryan@applecart.co>
2020-02-01 11:57:21 -0500

    Added Twitter button

--- src/containers/Quotebox.tsx
+++ src/containers/Quotebox.tsx
@@ -5,6 +5,7 @@ const Quotebox = () => {
   const [loading, setLoading] = useState(true);
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
+
   const getAndSetNewQuote = async () => {
     setLoading(true);
     const response = await fetch('https://quote-garden.herokuapp.com/quotes/random');
@@ -18,19 +19,23 @@ const Quotebox = () => {
     getAndSetNewQuote();
   }, []);
 
+  const tweetText = `${quote} -${author}`;
   return (
-    <div>
+    <div id="quote-box">
       {loading ? (
         <ClipLoader />
       ) : (
-        <div id="quote-box">
+        <>
           <p id="text">{quote}</p>
           {!!author && <p id="author">- {author}</p>}
-        </div>
+        </>
       )}
       <button id="new-quote" onClick={getAndSetNewQuote}>
         New Quote
       </button>
+      <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${tweetText}`}>
+        Twitter
+      </a>
     </div>
   );
 };
